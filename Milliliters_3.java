12d300f7ae5ecd9af1a2a209b3c72e180a76d636
muppidishivani <133558846+muppidishivani@users.noreply.github.com>
2024-04-11 17:13:19 -0700

    Add files via upload

---
+++ Milliliters_3.java
@@ -0,0 +1,14 @@
+package com.web.java;
+
+public class Milliliters_3 {
+
+	    public static void main(String[] args) {
+
+	        
+	double liters = 2.5678;
+		        double milliliters = liters * 1000;
+
+		        System.out.println("milliliters ="+milliliters );
+		    }
+	}
+
